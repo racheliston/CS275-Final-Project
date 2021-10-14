ab45776c9b6eb04c6fb34ec636c3491e33e95a4f
rliston <rliston@uvm.edu>
2021-10-14 10:57:14 -0400

    Added manager and patron buttons on main page

--- CS275-Final-Project/ViewController.swift
+++ CS275-Final-Project/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // buttons for main page for patron and manager
+    @IBOutlet var patronButton: UIButton!
+    @IBOutlet var managerButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    // sends user to map for patrons
+    @IBAction func patronView(_ sender: UIButton) {
+        
+    }
+    
+    // sends user to login page for managers
+    @IBAction func managerView(_ sender: UIButton) {
+        
+    }
 
 }
 
