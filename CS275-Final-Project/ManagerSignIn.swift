0cdcb34ef5466ea3736e8e28391d6def7377cf67
spitonya4 <spitonya@uvm.edu>
2021-10-18 15:29:21 -0400

    Got segue from manager button to ManagerSignInViewController

---
+++ CS275-Final-Project/ManagerSignIn.swift
@@ -0,0 +1,12 @@
+//
+//  ManagerSignIn.swift
+//  CS275-Final-Project
+//
+//  Created by Sam Pitonyak on 10/18/21.
+//
+
+import UIKit
+
+class ManagerSignInViewController: UIViewController {
+    
+}
