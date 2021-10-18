c00bc64d4dfd4c55815039f8070b266a8b55052c
spitonya4 <spitonya@uvm.edu>
2021-10-18 17:57:51 -0400

    Worked on manager settings page

---
+++ CS275-Final-Project/ManagerSettings.swift
@@ -0,0 +1,13 @@
+//
+//  ManagerSettings.swift
+//  CS275-Final-Project
+//
+//  Created by Sam Pitonyak on 10/18/21.
+//
+
+import UIKit
+
+class ManagerSettingsViewController: UIViewController {
+    
+    
+}
