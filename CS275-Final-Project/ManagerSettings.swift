657ed7be83da4829abc0d556b2a450903f9dba3a
spitonya4 <spitonya@uvm.edu>
2021-10-18 18:19:54 -0400

    Got it so when manager clicks sign out button, pops all views off the view controller and returns to the home page

--- CS275-Final-Project/ManagerSettings.swift
+++ CS275-Final-Project/ManagerSettings.swift
@@ -9,5 +9,12 @@ import UIKit
 
 class ManagerSettingsViewController: UIViewController {
     
+    // Make a button to sign out
+    @IBOutlet var signOutButton: UIButton!
     
+    // Pop all view controllers off the stack if sign out is pressed
+    @IBAction func signOut(_ sender: UIButton) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+
 }
