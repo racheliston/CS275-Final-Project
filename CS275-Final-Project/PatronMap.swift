481127540e8e421b9c9d12bc1f2785e2ce6ccad5
rliston <rliston@uvm.edu>
2021-10-18 16:40:20 -0400

    Began to make the map go directly to the user's location and changed privacy settings in info.plist

--- CS275-Final-Project/PatronMap.swift
+++ CS275-Final-Project/PatronMap.swift
@@ -7,62 +7,73 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class PatronMapViewController: UIViewController {
     
     var mapView: MKMapView!
-        override func loadView() {
-            // Create a map view
-            mapView = MKMapView()
-            // Set it as *the* view of this view controller
-            view = mapView
-            
-            // Labels for the menu to switch map types
-            let segmentedControl
-                        = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
-                segmentedControl.backgroundColor = UIColor.systemBackground
-                segmentedControl.selectedSegmentIndex = 0
-                
-                // Attach a target-action pair to segmented control
-                segmentedControl.addTarget(self,
-                                           action: #selector(mapTypeChanged(_:)),
-                                           for: .valueChanged)
-            
-                // idk yet
-                segmentedControl.translatesAutoresizingMaskIntoConstraints = false
-                view.addSubview(segmentedControl)
-            
-            // Margins
-            let topConstraint =
-                segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
-            let margins = view.layoutMarginsGuide
-            let leadingConstraint =
-                segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
-            let trailingConstraint =
-                segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
-            
-            topConstraint.isActive = true
-            leadingConstraint.isActive = true
-            trailingConstraint.isActive = true
-            
-        }
+    fileprivate let locationManager:CLLocationManager = CLLocationManager()
     
-        // Function to switch map types
-        @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
-            switch segControl.selectedSegmentIndex {
-            case 0:
-                mapView.mapType = .standard
-            case 1:
-                mapView.mapType = .hybrid
-            case 2:
-                mapView.mapType = .satellite
-            default:
-        break }
-        }
-    
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            print("PatronMapViewController loaded its view.")
-        }
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        // Set it as *the* view of this view controller
+        view = mapView
+        
+        // Labels for the menu to switch map types
+        let segmentedControl
+                    = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+            segmentedControl.backgroundColor = UIColor.systemBackground
+            segmentedControl.selectedSegmentIndex = 0
+            
+            // Attach a target-action pair to segmented control
+            segmentedControl.addTarget(self,
+                                       action: #selector(mapTypeChanged(_:)),
+                                       for: .valueChanged)
+        
+            segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(segmentedControl)
+        
+        // Margins
+        let topConstraint =
+            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8)
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint =
+            segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint =
+            segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
+
+    // Function to switch map types
+    @objc func mapTypeChanged(_ segControl: UISegmentedControl) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            mapView.mapType = .standard
+        case 1:
+            mapView.mapType = .hybrid
+        case 2:
+            mapView.mapType = .satellite
+        default:
+    break }
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        locationManager.startUpdatingLocation()
+        
+        mapView.showsUserLocation = true
+        
+        print("PatronMapViewController loaded its view.")
+    }
     
 }
