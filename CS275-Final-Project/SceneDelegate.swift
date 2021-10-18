1205d45125a4cd78e001893ccc2adb3909a4aabd
spitonya4 <spitonya@uvm.edu>
2021-10-18 17:21:27 -0400

    Got back buttons up with Navigation controller

--- CS275-Final-Project/SceneDelegate.swift
+++ CS275-Final-Project/SceneDelegate.swift
@@ -17,6 +17,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        let navController = window!.rootViewController as! UINavigationController
+        _ = navController.topViewController as! ViewController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
