77dce8bba5ef0c0314b07c7fd01e38186f4ca37d
spitonya4 <spitonya@uvm.edu>
2021-10-18 16:40:13 -0400

    Connecting labels and buttons to manager home controller

--- CS275-Final-Project/ManagerHome.swift
+++ CS275-Final-Project/ManagerHome.swift
@@ -12,14 +12,46 @@ class ManagerHomeViewController: UIViewController {
     // Labels for home
     @IBOutlet var capacityLabel: UILabel!
     @IBOutlet var lineLabel: UILabel!
-
+    
+    // Buttons for home
+    @IBOutlet var capacityMinusButton: UIButton!
+    @IBOutlet var capacityPlusButton: UIButton!
+    @IBOutlet var lineMinusButton: UIButton!
+    @IBOutlet var linePlusButton: UIButton!
+    
+    var totalCapacityValue: Int = 0
+    var capacityValue: Int = 0
+    var lineValue: Int = 0
+    
     
     // For start view for capacity and label
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        capacityLabel.text = "0"
+        // HARDCODED CAP VALUE
+        totalCapacityValue = 50
+        
+        capacityLabel.text = "0 / \(totalCapacityValue)"
         lineLabel.text = "0"
         
+    }
+    
+    // Increment capacity value if addition symbol is pressed
+    @IBAction func incrementCap(_ sender: UIButton) {
+        // Increment the value
+        capacityValue += 1
+        
+        capacityLabel.text = String(capacityValue) + " / \(totalCapacityValue)"
+    }
+    
+    @IBAction func decrementCap(_ sender: UIButton) {
+        // Make sure capacity is not less than 0
+        if capacityValue > 0 {
+            // Decrement the value
+            capacityValue -= 1
+        }
+                
+        
+        capacityLabel.text = String(capacityValue) + " / \(totalCapacityValue)"
     }
 }
